c3e9edce0d7fd57fe5d3876983630d33f694c1f7
Jackson Ming Hu <huming2207@gmail.com>
2020-06-06 16:19:26 +1000

    Add constants

---
+++ src/common/constants.rs
@@ -0,0 +1,7 @@
+pub const EMPTY: &str = "";
+
+// dotenv config keys
+pub const DB_URL: &str = "MONGODB_URL";
+pub const LISTEN_ADDR: &str = "LISTEN_ADDRESS";
+pub const JWT_SECRET: &str = "JWT_SECRET";
+pub const JWT_EXPIRE_HR: &str = "JWT_EXPIRES_HR";
