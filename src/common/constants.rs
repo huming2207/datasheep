5d3ad35f36ee6980454ba3e56b5655326c308494
Jackson Ming Hu <huming2207@gmail.com>
2020-06-06 22:24:55 +1000

    constants: add password salt constant

--- src/common/constants.rs
+++ src/common/constants.rs
@@ -5,3 +5,4 @@ pub const DB_URL: &str = "MONGODB_URL";
 pub const LISTEN_ADDR: &str = "LISTEN_ADDRESS";
 pub const JWT_SECRET: &str = "JWT_SECRET";
 pub const JWT_EXPIRE_HR: &str = "JWT_EXPIRES_HR";
+pub const PASSWORD_SALT: &str = "PASSWORD_SALT";
