f6131afa52a58a747691b61b420cddc8260d99f2
Jackson Ming Hu <huming2207@gmail.com>
2020-06-06 22:27:25 +1000

    user: use as_bytes instead

--- src/models/user.rs
+++ src/models/user.rs
@@ -17,10 +17,10 @@ impl User {
     pub fn set_password(&mut self, raw: &str) {
         let salt = env::var(constants::PASSWORD_SALT).unwrap();
         let config = Config::default();
-        self.password = argon2::hash_encoded(raw.as_ref(), salt.as_ref(), &config).unwrap();
+        self.password = argon2::hash_encoded(raw.as_ref(), salt.as_bytes(), &config).unwrap();
     }
 
-    pub fn compare_password(&self, raw_passwd: &str) -> bool {
-        return argon2::verify_encoded(self.password.as_str(), raw_passwd.as_ref()).unwrap();
+    pub fn compare_password(&self, raw: &str) -> bool {
+        return argon2::verify_encoded(self.password.as_str(), raw.as_bytes()).unwrap();
     }
 }
