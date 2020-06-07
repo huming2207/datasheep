3d8ec0b0ac513c490b8034703e32e7c5f12b18fe
Jackson Ming Hu <huming2207@gmail.com>
2020-06-07 11:30:33 +1000

    Use actix-web-httpauth middleware instead

--- src/middleware/jwt_validator.rs
+++ src/middleware/jwt_validator.rs
@@ -1,44 +1,29 @@
-use crate::helpers::jwt;
-use crate::helpers::jwt::{validate_token, JwtClaims};
-use actix_web::dev::{Payload, PayloadStream, ServiceRequest};
+use crate::helpers::jwt::validate_token;
+use actix_web::dev::ServiceRequest;
 use actix_web::error::ErrorUnauthorized;
-use actix_web::http::HeaderValue;
-use actix_web::{dev, FromRequest, HttpRequest};
+use actix_web::{Error, HttpMessage};
 use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
-use actix_web_httpauth::extractors::AuthenticationError;
-use actix_web_httpauth::middleware::HttpAuthentication;
-use futures::future::{err, ok, Ready};
-use log::Level::Error;
-use std::borrow::Borrow;
-use std::io;
 
 #[derive(Debug, Deserialize)]
 struct UserContext {
     id: String,
 }
 
-impl FromRequest for UserContext {
-    type Error = actix_web::Error;
-    type Future = Ready<Result<Self, Self::Error>>;
-    type Config = ();
-
-    fn from_request(req: &HttpRequest, payload: &mut dev::Payload) -> Self::Future {
-        match req.headers().get("Authorization") {
-            None => err(ErrorUnauthorized("No Authorization found")),
-            Some(value) => {
-                let bearer_token = value.to_str()?;
-                if bearer_token.starts_with("Bearer") || bearer_token.starts_with("bearer") {
-                    let token = bearer_token[6..bearer_token.len()].trim();
-                    match validate_token(token) {
-                        Ok(claim) => ok(UserContext {
-                            id: claim.uid.clone(),
-                        }),
-                        Err(_) => err(ErrorUnauthorized("Invalid token")),
-                    }
-                } else {
-                    err(ErrorUnauthorized("Token is not a bearer token"))
-                }
-            }
+pub async fn jwt_validator(
+    req: ServiceRequest,
+    credentials: BearerAuth,
+) -> Result<ServiceRequest, Error> {
+    let config = req
+        .app_data::<Config>()
+        .map(|data| data.get_ref().clone())
+        .unwrap_or_else(Default::default);
+    match validate_token(credentials.token()) {
+        Ok(res) => {
+            req.extensions_mut().insert(UserContext {
+                id: res.uid.clone(),
+            });
+            Ok(req)
         }
+        Err(err) => Err(ErrorUnauthorized(err)),
     }
 }
