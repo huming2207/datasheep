2e537e7f330e3c940abdb5ca1a50089b8869753f
Jackson Ming Hu <huming2207@gmail.com>
2020-06-07 10:53:38 +1000

    constants: add back db_name

--- src/common/constants.rs
+++ src/common/constants.rs
@@ -2,6 +2,7 @@ pub const EMPTY: &str = "";
 
 // dotenv config keys
 pub const DB_URL: &str = "MONGODB_URL";
+pub const DB_NAME: &str = "MONGODB_NAME";
 pub const LISTEN_ADDR: &str = "LISTEN_ADDRESS";
 pub const JWT_SECRET: &str = "JWT_SECRET";
 pub const JWT_EXPIRE_HR: &str = "JWT_EXPIRES_HR";
