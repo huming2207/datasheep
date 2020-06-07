e44f7f80f4c1a03a47ed008ba3419a325b7f9c68
Jackson Ming Hu <huming2207@gmail.com>
2020-06-07 10:45:51 +1000

    main: add database connector

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,21 @@
 extern crate log;
 extern crate argon2;
 
+#[macro_use]
+extern crate bson;
+
+#[macro_use]
+extern crate validator_derive;
+extern crate validator;
+#[macro_use]
+extern crate serde_derive;
+extern crate mongodb;
+extern crate serde_json;
+
 use crate::common::constants;
-use crate::middleware::jwt_validator;
 use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer};
+use mongodb::Client;
 use std::{env, io};
 
 mod common;
@@ -25,10 +36,21 @@ async fn main() -> io::Result<()> {
         debug!("{}: {}", key, val);
     }
 
+    let db_client = match Client::with_uri_str(env::var(constants::DB_URL).unwrap().as_str()).await
+    {
+        Ok(client) => Ok(client),
+        Err(err) => Err(io::Error::new(
+            io::ErrorKind::NotConnected,
+            "Failed when connecting to database",
+        )),
+    }?;
+
+    let db_database = db_client.database(env::var(constants::DB_NAME).unwrap().as_str());
+
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .wrap(jwt_validator::validator)
+            .data(db_database.clone())
             .configure(server::load_services)
     })
     .bind(env::var(constants::LISTEN_ADDR).unwrap().as_str())?
